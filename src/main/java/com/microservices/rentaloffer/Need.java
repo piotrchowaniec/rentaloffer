c2c6fd24f8d72f50bc87471f0041c323bdcdad88
Piotr Chowaniec <piotr.chowaniec@fingo.pl>
2015-09-10 10:50:48 +0200

    Initial commit

---
+++ src/main/java/com/microservices/rentaloffer/Need.java
@@ -0,0 +1,28 @@
+package com.microservices.rentaloffer;
+
+import java.io.IOException;
+
+public class Need {
+
+    public static void main(String[] args) {
+        String host = args[0];
+        String busName = args[1];
+
+        Need.publish(host, busName);
+    }
+
+    public static void publish(String host, String busName) {
+        while(true)
+        {
+            try (Connections connection = new Connections( host, busName ))
+            {
+                connection.publish( new NeedPacket().toJson() );
+                Thread.sleep( 5000 );
+            }
+            catch( Exception e )
+            {
+                throw new RuntimeException( "Could not publish message:", e );
+            }
+        }
+    }
+}
